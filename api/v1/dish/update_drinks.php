67b5dc203ccd80cc37f2a8b9a6e55fa9707a240b
Khánh Võ An <voankhanh2k2@gmail.com>
2024-03-25 17:57:25 +0700

    modify update_drinks

--- api/v1/dish/update_drinks.php
+++ api/v1/dish/update_drinks.php
@@ -4,6 +4,40 @@ require_once('../db_connect.php');
 if ($_SERVER['REQUEST_METHOD'] === 'PUT') {
     $data = json_decode(file_get_contents("php://input"), true);
 
+    if (isset($data['DrinkName'])) {
+        $drinkName = $data['DrinkName'];
+        $specialChars = array("~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "{", "]", "}", "\\", "|", ";", ":", "'", "\"", "<", ">", ",", ".", "?", "/");
+        $invalidCharsFound = false;
+        foreach ($specialChars as $char) {
+            if (strpos($drinkName, $char) !== false) {
+                $invalidCharsFound = true;
+                break;
+            }
+        }
+        if ($invalidCharsFound) {
+            $response = array('message' => 'Drink name should not contain special characters', 'status' => 'error');
+            echo json_encode($response);
+            exit;
+        }
+    }
+
+    if (isset($data['Description'])) {
+        $description = $data['Description'];
+        $specialChars = array("~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "{", "]", "}", "\\", "|", ";", ":", "'", "\"", "<", ">", ",", ".", "?", "/");
+        $invalidCharsFound = false;
+        foreach ($specialChars as $char) {
+            if (strpos($description, $char) !== false) {
+                $invalidCharsFound = true;
+                break;
+            }
+        }
+        if ($invalidCharsFound) {
+            $response = array('message' => 'Description should not contain special characters', 'status' => 'error');
+            echo json_encode($response);
+            exit;
+        }
+    }
+
     if (isset($data['DrinksID']) && isset($data['DrinkName']) && isset($data['Price']) && isset($data['Description']) && isset($data['Image']) && isset($data['MenuID'])) {
         $drinksID = $data['DrinksID'];
         $drinksName = $data['DrinkName'];

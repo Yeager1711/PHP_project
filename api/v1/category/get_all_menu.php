febf8a43d09cd39f76b9dbb5c2522417f28b45dd
Khánh Võ An <voankhanh2k2@gmail.com>
2024-03-25 19:18:35 +0700

    modify get_drink and get_all_menu

--- api/v1/category/get_all_menu.php
+++ api/v1/category/get_all_menu.php
@@ -1,8 +1,7 @@
 <?php
 require_once('../db_connect.php');
-// Xử lý phương thức GET
+
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-    // Tạo câu truy vấn SELECT
     $sql = "SELECT * FROM menu";
     $result = $conn->query($sql);
     $response = array();
